426bcba944823de5cce91bed0d4bc0b7790b2f57
manasa y <123yapitsme@gmail.com>
2025-03-15 20:57:23 +0530

    doing the about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function AboutMe() {
   return (
     <div>
-      <h1>hiiiii</h1>
+      <h1>Know who i am</h1>
+      <p>Hi, I'm XYZ, a passionate web developer currently pursuing my engineering degree at XYZ. I love bringing ideas to life by turning concepts into functional, user-friendly websites. My recent project, an AI-powered sustainable upcycling assistant built with Next.js and TypeScript, reflects my enthusiasm for combining technology with impactful solutions.
+
+With expertise in Next.js, React.js, TypeScript, Python, and JavaScript, I enjoy solving coding challenges and continuously learning new technologies. Creativity and persistence drive me to build innovative and efficient web applications.
+
+I'm always eager to take on new challenges and collaborate on exciting projects. Let’s connect!
+
+</p>
+<h2>Skillset</h2>
+
     </div>
   )
 }
